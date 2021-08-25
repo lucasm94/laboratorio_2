733d272cce60af72a5d37b7bd7b33cbeb90bf0c1
Lucas <lucas.monzon@mitrol.net>
2021-08-24 21:11:10 -0300

    Ejercicio 1

---
+++ clase-2/Ejercicio1/ValidadorRangos/Program.cs
@@ -0,0 +1,60 @@
+﻿using Biblioteca;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidadorRangos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            int min = -100;
+            int max = 100;
+            bool esNumerico;
+            int minimoAgregado = max;
+            int maximoAgregado = min;
+            int acumNumerosIngresados = 0;
+            float acumuladoValor = 0;
+
+            while (acumNumerosIngresados < 10)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                esNumerico = int.TryParse(Console.ReadLine(), out valor);
+                if (esNumerico)
+                {
+                    if (Validador.Validar(valor, min, max))
+                    {
+                        if (minimoAgregado > valor)
+                        {
+                            minimoAgregado = valor;
+                        }
+                        if (maximoAgregado < valor)
+                        {
+                            maximoAgregado = valor;
+                        }
+                        acumuladoValor += valor;
+                        Console.WriteLine("Esta dentro del rango");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Esta fuera del rango");
+                    }
+                    acumNumerosIngresados++;
+                }
+                else
+                {
+                    Console.WriteLine("Lo ingresado no es un numero");
+                }
+            }
+
+            Console.WriteLine("El numero minimo ingresado es {0}", minimoAgregado);
+            Console.WriteLine("El numero maximo ingresado es {0}", maximoAgregado);
+            Console.WriteLine("El promedio es {0}", acumuladoValor/10);
+            Console.ReadLine();
+        }
+    }
+}
