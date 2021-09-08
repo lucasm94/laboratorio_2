c8ade36c31b1bfd9b78212664abf9ba5b2676dd9
Lucas <lucas.monzon@mitrol.net>
2021-09-07 22:17:00 -0300

    contador palabras

---
+++ clase-6/Ejercicio-I03-Contador-palabras/Ejercicio-I03/ContadorForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Ejercicio_I03
+{
+    partial class ContadorForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtForm = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtxtForm
+            // 
+            this.rtxtForm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxtForm.Location = new System.Drawing.Point(2, 0);
+            this.rtxtForm.Name = "rtxtForm";
+            this.rtxtForm.Size = new System.Drawing.Size(644, 410);
+            this.rtxtForm.TabIndex = 0;
+            this.rtxtForm.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalcular.Location = new System.Drawing.Point(520, 417);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(119, 31);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // ContadorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 460);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rtxtForm);
+            this.Name = "ContadorForm";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtForm;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ clase-6/Ejercicio-I03-Contador-palabras/Ejercicio-I03/ContadorForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_I03
+{
+    public partial class ContadorForm : Form
+    {
+        public ContadorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            string[] palabras = rtxtForm.Text.Split(' ', (char) StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> diccPalabras = CuentaPalabras(palabras);
+            List<KeyValuePair<string, int>> cuentaPalabrasList = diccPalabras.ToList();
+            cuentaPalabrasList.Sort(CompararCantidadRepeticiones);
+            MostrarPalabras(cuentaPalabrasList);
+        }
+
+        private Dictionary<string, int> CuentaPalabras(string[] palabras)
+        {
+            Dictionary<string, int> diccPalabras = new Dictionary<string, int>();
+            foreach (string palabra in palabras)
+            {
+                if (diccPalabras.ContainsKey(palabra))
+                {
+                    diccPalabras[palabra]++;
+                }
+                else
+                {
+                    diccPalabras.Add(palabra, 1);
+                }
+            }
+            return diccPalabras;
+        }
+
+        private int CompararCantidadRepeticiones(KeyValuePair<string, int> palabraUno, 
+            KeyValuePair<string, int> palabraDos)
+        {
+            return palabraDos.Value - palabraUno.Value;
+        }
+
+        private void MostrarPalabras(List<KeyValuePair<string, int>> palabras)
+        {
+            if (palabras.Count == 0)
+            {
+                MessageBox.Show("No se ingresaron palabras");
+            } else
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 4 && i < palabras.Count; i++)
+                {
+                    sb.AppendLine($"Palabra: {palabras[i].Key} - Valor: {palabras[i].Value}");
+                }
+                MessageBox.Show(sb.ToString());
+            }
+        }
+    }
+}
